ac5e319f32700ada3b45d6f25925ad0145949080
yogeshsmindfire <yogeshs@mindfiresolutions.com>
2025-10-28 20:55:46 +0530

    fix: add displayName to mocked components in page test

--- src/app/__tests/page.test.tsx
+++ src/app/__tests/page.test.tsx
@@ -2,15 +2,51 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import Page from '../page';
 
 // Mock all the child components to isolate the Page component tests
-jest.mock('@/app/components/HeroModule/HeroModule', () => () => <div>HeroModule</div>);
-jest.mock('@/app/components/NavigationBar/Navigationbar', () => ({ scrollPosition }) => <div>NavigationBar scroll: {scrollPosition}</div>);
-jest.mock('@/app/components/Clients/Clients', () => () => <div>Clients</div>);
-jest.mock('@/app/components/Services/Services', () => () => <div>Services</div>);
-jest.mock('@/app/components/ClientFeedback/ClientFeedback', () => () => <div>ClientFeedback</div>);
-jest.mock('@/app/components/Footer/Footer', () => () => <div>Footer</div>);
-jest.mock('@/app/components/CustomerInsights/CustomerInsights', () => () => <div>CustomerInsights</div>);
-jest.mock('@/app/components/Technologies/Technologies', () => () => <div>Technologies</div>);
-jest.mock('@/app/components/SuccessStories/SuccessStories', () => () => <div>SuccessStories</div>);
+jest.mock('@/app/components/HeroModule/HeroModule', () => {
+  const MockHeroModule = () => <div>HeroModule</div>;
+  MockHeroModule.displayName = 'HeroModule';
+  return MockHeroModule;
+});
+jest.mock('@/app/components/NavigationBar/Navigationbar', () => {
+  const MockNavigationBar = ({ scrollPosition }: { scrollPosition: number }) => <div>NavigationBar scroll: {scrollPosition}</div>;
+  MockNavigationBar.displayName = 'NavigationBar';
+  return MockNavigationBar;
+});
+jest.mock('@/app/components/Clients/Clients', () => {
+  const MockClients = () => <div>Clients</div>;
+  MockClients.displayName = 'Clients';
+  return MockClients;
+});
+jest.mock('@/app/components/Services/Services', () => {
+  const MockServices = () => <div>Services</div>;
+  MockServices.displayName = 'Services';
+  return MockServices;
+});
+jest.mock('@/app/components/ClientFeedback/ClientFeedback', () => {
+  const MockClientFeedback = () => <div>ClientFeedback</div>;
+  MockClientFeedback.displayName = 'ClientFeedback';
+  return MockClientFeedback;
+});
+jest.mock('@/app/components/Footer/Footer', () => {
+  const MockFooter = () => <div>Footer</div>;
+  MockFooter.displayName = 'Footer';
+  return MockFooter;
+});
+jest.mock('@/app/components/CustomerInsights/CustomerInsights', () => {
+  const MockCustomerInsights = () => <div>CustomerInsights</div>;
+  MockCustomerInsights.displayName = 'CustomerInsights';
+  return MockCustomerInsights;
+});
+jest.mock('@/app/components/Technologies/Technologies', () => {
+  const MockTechnologies = () => <div>Technologies</div>;
+  MockTechnologies.displayName = 'Technologies';
+  return MockTechnologies;
+});
+jest.mock('@/app/components/SuccessStories/SuccessStories', () => {
+  const MockSuccessStories = () => <div>SuccessStories</div>;
+  MockSuccessStories.displayName = 'SuccessStories';
+  return MockSuccessStories;
+});
 
 describe('Page', () => {
   it('renders all the child components', () => {
